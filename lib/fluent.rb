fa9503e7af619003eaccec0e8d58f9c3e868f7f3
Jeff Nyman <jeffnyman@gmail.com>
2013-10-13 06:07:06 -0500

    Fluent recognizes it has been included.

--- lib/fluent.rb
+++ lib/fluent.rb
@@ -5,5 +5,16 @@ module Fluent
   def self.version
     "Fluent v#{Fluent::VERSION}"
   end
+
+  # The included callback is used to provide the core functionality of the
+  # library to any class or module that includes the Fluent library. The
+  # calling class or module is extended with logic that the library makes
+  # available as class methods. Those classes become page definitions or
+  # activity definitions.
+  #
+  # @param caller [Class] the class including the library
+  def self.included(caller)
+    puts 'Fluent has been included.'
+  end
   
 end
