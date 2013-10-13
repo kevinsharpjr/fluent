84bd9e28ef2df59f9bd9e0cddc71538a9592e8ad
Jeff Nyman <jeffnyman@gmail.com>
2013-10-13 07:43:20 -0500

    Browser driver is attached with Fluent.

--- lib/fluent.rb
+++ lib/fluent.rb
@@ -1,6 +1,9 @@
 require 'fluent/version'
 require 'fluent/logger'
 
+require 'watir-webdriver'
+require 'selenium-webdriver'
+
 module Fluent
   
   def self.version
@@ -16,6 +19,15 @@ module Fluent
   # @param caller [Class] the class including the library
   def self.included(caller)
     Fluent.trace("#{caller.class} #{caller} is now Fluent.")
+  end
+
+  # @param browser [Object] a browser instance with a tool driver
+  def initialize(browser=nil)
+    @browser = browser
+    @browser = Watir::Browser.new if browser.nil? or browser == :watir
+    @browser = Selenium::WebDriver.for :firefox if browser == :selenium
+
+    Fluent::trace("Fluent attached to browser: #{@browser}")
   end
   
 end
