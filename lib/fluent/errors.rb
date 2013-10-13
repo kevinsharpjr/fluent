3b00d9894e847f973cd176454a2e9c13d86e971b
Jeff Nyman <jeffnyman@gmail.com>
2013-10-13 09:27:41 -0500

    Check title generator allows page titles to be validated.

--- lib/fluent/errors.rb
+++ lib/fluent/errors.rb
@@ -1,5 +1,6 @@
 module Fluent
 
   class NoUrlForDefinition < StandardError; end
+  class TitleNotMatched < StandardError; end
   
 end
--- lib/fluent/generators.rb
+++ lib/fluent/generators.rb
@@ -10,5 +10,14 @@ module Fluent
       end
     end
     
+    def title_is(title=nil)
+      define_method('check_title') do
+        msg  = "Expected title: '#{title}'; Actual title: '#{browser.title}'"
+        valid_title = title == browser.title if title.kind_of?(String)
+        raise Fluent::TitleNotMatched, msg unless valid_title
+        valid_title
+      end
+    end
+    
   end
 end
