e0a3944790adac3acb35c2b09e9a627bf6664296
Jeff Nyman <jeffnyman@gmail.com>
2013-10-19 14:23:23 -0500

    All actionable locators are cloned.

--- lib/fluent/generators.rb
+++ lib/fluent/generators.rb
@@ -31,7 +31,7 @@ module Fluent
     
     def link(identifier, locator)
       define_method(identifier) do
-        return platform.link_click(locator)
+        return platform.link_click(locator.clone)
       end
       
       common_definition_methods(identifier, locator, __method__)
@@ -39,7 +39,7 @@ module Fluent
     
     def paragraph(identifier, locator)
       define_method(identifier) do
-        return platform.paragraph_text(locator)
+        return platform.paragraph_text(locator.clone)
       end
       
       common_definition_methods(identifier, locator, __method__)
@@ -47,7 +47,7 @@ module Fluent
     
     def button(identifier, locator)
       define_method(identifier) do
-        return platform.button_click(locator)
+        return platform.button_click(locator.clone)
       end
       
       common_definition_methods(identifier, locator, __method__)
@@ -55,11 +55,11 @@ module Fluent
     
     def text_field(identifier, locator)
       define_method(identifier) do
-        return platform.text_field_get(locator)
+        return platform.text_field_get(locator.clone)
       end
       
       define_method("#{identifier}=") do |value|
-        return platform.text_field_set(locator, value)
+        return platform.text_field_set(locator.clone, value)
       end
       
       common_definition_methods(identifier, locator, __method__)
@@ -67,15 +67,15 @@ module Fluent
     
     def checkbox(identifier, locator)
       define_method("#{identifier}_checked?") do
-        return platform.checkbox_check_state(locator)
+        return platform.checkbox_check_state(locator.clone)
       end
 
       define_method("check_#{identifier}") do
-        return platform.checkbox_check(locator)
+        return platform.checkbox_check(locator.clone)
       end
 
       define_method("uncheck_#{identifier}") do
-        return platform.checkbox_uncheck(locator)
+        return platform.checkbox_uncheck(locator.clone)
       end
       
       common_definition_methods(identifier, locator, __method__)
@@ -83,13 +83,13 @@ module Fluent
     
     def select_list(identifier, locator)
       define_method(identifier) do
-        return platform.select_list_get_selected(locator)
+        return platform.select_list_get_selected(locator.clone)
       end
 
       alias_method "#{identifier}_option?".to_sym, "#{identifier}".to_sym
       
       define_method("#{identifier}=") do |value|
-        return platform.select_list_set(locator, value)
+        return platform.select_list_set(locator.clone, value)
       end
 
       define_method("#{identifier}_options?") do
@@ -98,7 +98,7 @@ module Fluent
       end
 
       define_method("#{identifier}_value?") do
-        return platform.select_list_get_value(locator)
+        return platform.select_list_get_value(locator.clone)
       end
       
       common_definition_methods(identifier, locator, __method__)
@@ -106,11 +106,11 @@ module Fluent
     
     def radio(identifier, locator)
       define_method("select_#{identifier}") do
-        return platform.radio_select(locator)
+        return platform.radio_select(locator.clone)
       end
 
       define_method("#{identifier}_selected?") do
-        return platform.radio_check_state(locator)
+        return platform.radio_check_state(locator.clone)
       end
 
       alias_method "#{identifier}_set?".to_sym, "#{identifier}_selected?".to_sym
@@ -123,15 +123,15 @@ module Fluent
     
     def common_definition_methods(identifier, locator, method)
       define_method("#{identifier}_object") do
-        platform.send(method, locator)
+        platform.send(method, locator.clone)
       end
       
       define_method("#{identifier}_exists?") do
-        platform.send(method, locator).exists?
+        platform.send(method, locator.clone).exists?
       end
 
       define_method("#{identifier}_visible?") do
-        platform.send(method, locator).visible?
+        platform.send(method, locator.clone).visible?
       end
             
       alias_method "#{identifier}_#{method}".to_sym, "#{identifier}_object".to_sym
@@ -148,7 +148,7 @@ module Fluent
       
       if Fluent.can_be_enabled?(method)
         define_method("#{identifier}_enabled?") do
-          platform.send(method, locator).enabled?
+          platform.send(method, locator.clone).enabled?
         end
 
         alias_method "#{identifier}!".to_sym, "#{identifier}_enabled?".to_sym
