7333554caa69f6a74223764dbb3ffb0df5c364fe
Jeff Nyman <jeffnyman@gmail.com>
2013-10-19 06:43:14 -0500

    Factory is dumb about actual browser.

--- lib/fluent/factory.rb
+++ lib/fluent/factory.rb
@@ -2,15 +2,7 @@ module Fluent
   module Factory
     
     def on(definition, visit=false, &block)
-      puts "ON: browser = #{@browser}; active = #{@active}; platform = #{@platform}"
-      active_browser = :watir if @browser.nil?
-      
-      #browser = Watir::Browser.new if @browser.nil? or @browser == :watir
-      #@browser = Selenium::WebDriver.for :firefox if @browser == :selenium
-      #@browser = Watir::Browser.new if not @browser.kind_of?(Watir::Browser)
-      
       return @active if @active.kind_of?(definition)
-      #@active = definition.new(@browser, visit)
       @active = definition.new(@browser, visit)
       block.call @active if block
       @active
@@ -20,7 +12,6 @@ module Fluent
     alias_method :while_on, :on
     
     def on_view(definition, &block)
-      #puts "ON_VIEW: browser = #{@browser}; active = #{@active}; platform = #{@platform}"
       on(definition, true, &block)
     end
 
